eda9c59cb0fc9ef931f9c215f407d9411e7be0fa
David Vloedman <djvloedman@gmail.com>
2019-11-12 12:25:30 -0800

    Got URL string configured for api hit

--- server.js
+++ server.js
@@ -5,19 +5,37 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
-const path = require('path');
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views/pages'));
-
-app.get('/', (request, response) => {
-  response.render('index');
-});
 
 
+app.get('/', newSearch);
+
+function newSearch(request, response) {
+  response.render('pages/index');
+}
+
+app.post('/searches', createSearch);
+
+// Search
+
+function createSearch(request, response) {
+  let url = `https://www.googleapis.com/books/v1/volumes?q=`;
+  
+  if (request.body.search[1] === 'title') url += `+intitle:${request.body.search[0]}`;
+  if (request.body.search[1] === 'author') url += `+inauthor:${request.body.search[0]}`;
+  console.log(url);
+  // superagent.get(url)
+  //   .then(results => results.body.items.forEach(book => console.log(book.volumeInfo)));
+    
+
+  
+}
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
