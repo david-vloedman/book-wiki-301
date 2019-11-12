c9d0c5aa4375c6e46426d97300e9127cb5efdd83
David Vloedman <djvloedman@gmail.com>
2019-11-12 14:51:35 -0800

    Finishing out lab

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const Book = require('./api_modules/book.js');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -23,19 +24,22 @@ function newSearch(request, response) {
 
 app.post('/searches', createSearch);
 
-// Search
+app.get('*', (request, response) => response.status(404).send('This route does not exist'));
+
+function showError(request, response) {
+  response.render('pages/error');
+}
+
+
 
 function createSearch(request, response) {
   let url = `https://www.googleapis.com/books/v1/volumes?q=`;
   
   if (request.body.search[1] === 'title') url += `+intitle:${request.body.search[0]}`;
-  if (request.body.search[1] === 'author') url += `+inauthor:${request.body.search[0]}`;
-  console.log(url);
-  // superagent.get(url)
-  //   .then(results => results.body.items.forEach(book => console.log(book.volumeInfo)));
-    
-
-  
+  if (request.body.search[1] === 'author') url += `+inauthor:${request.body.search[0]}`;  
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(book => new Book(book.volumeInfo)))
+    .then(results => response.render('pages/show', { searchResults: results }));  
 }
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
